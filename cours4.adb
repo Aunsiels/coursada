0ec6a6dbb01afe63fccae3321cef6b08bd49a92e
Julien Romero <romerojulien34@gmail.com>
2014-12-31 16:13:02 +0100

    better task method

--- cours4.adb
+++ cours4.adb
@@ -2,41 +2,19 @@ with Ada.Real_Time; use Ada.Real_Time;
 
 package body Cours4 is
 
---Definition d'une task avec une priority de 20
-  task Blink1 is
-    pragma Priority (20);
-  end Blink1;
-
 --Corps de la tache
 
-  task body Blink1 is
+  task body Blink is
   begin
 --faire clignoter une led
-   Setup_Out_Pin(Pin1);
+   Setup_Out_Pin(Pin.all);
     loop
-      Set_Pin(Pin1,True);
+      Set_Pin(Pin.all,True);
       delay until Clock + To_Time_Span (0.5);
-      Set_Pin(Pin1,False);
+      Set_Pin(Pin.all,False);
       delay until Clock + To_Time_Span (0.5);
     end loop;
-  end Blink1;
-
---Idem pour une autre led
-
-  task Blink4 is
-    pragma Priority (20);
-  end Blink4;
-
-  task body Blink4 is
-  begin
-   Setup_Out_Pin(Pin4);
-    loop
-      Set_Pin(Pin4,True);
-      delay until Clock + To_Time_Span (1.0);
-      Set_Pin(Pin4,False);
-      delay until Clock + To_Time_Span (1.0);
-    end loop;
-  end Blink4;
+  end Blink;
 
 --une boucle de faisant rien d'autre qu'attendre
   procedure rien is
--- cours4.ads
+++ cours4.ads
@@ -2,10 +2,18 @@ with Stm32.RCC; use Stm32.RCC;
 with Stm32.GPIO; use Stm32.GPIO;
 pragma Elaborate_All (Stm32.GPIO);
 
-
 package Cours4 is
+
+--Definition d'une task avec une priority de 20
+  task type Blink (Pin : access Pin_Type) is
+    pragma Priority (20);
+  end Blink;
+
 --Declarer procedure et pins
   procedure rien;
-  Pin4 : constant Pin_Type := (GPIOE, 4);
-  Pin1 : constant Pin_Type := (GPIOE, 1);
+  Pin0 : aliased Pin_Type := (GPIOD, 12);
+  Pin1 : aliased Pin_Type := (GPIOD, 13);
+
+  Blk1 : Blink(Pin1'Access);
+  Blk0 : Blink(Pin0'Access);
 end Cours4;
